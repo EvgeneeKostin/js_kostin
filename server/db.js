bd746241d742e92facab09d73d1d61d922b86ddd
EvgeneeKostin <155514599+EvgeneeKostin@users.noreply.github.com>
2024-02-12 17:33:17 +1000

    Initial commit

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require('sequelize')
+
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mariadb',
+        port: process.env.DB_PORT
+    }
+)
---
+++ server/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+start()
