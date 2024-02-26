2a5c4ea9379ddbf009e2e7f90e32b4b48956fb0f
Zhenkad <evgenii200127@gmail.com>
2024-02-26 18:10:56 +1000

    .

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const { query } = require('express')
 const ApiError = require('../error/ApiErrors')
 const { User } = require('../models/models')
+const bcrypt = require('bcrypt')
 
 class UserController {
     async registration(req, res, next){
@@ -12,12 +13,18 @@ class UserController {
         if (candidate){
             return next(ApiError.badRequest('Такой пользователь уже существует.'))
         }
-        const user = await User.create({email, password})
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, password: hashPassword})
         return res.json({message: user})
     }
 
-    async login(req, res){
-
+    async login(req, res, next){
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email, password}})
+        if (!user){
+            return next(ApiError.internal('Неверный eMail или пароль.'))
+        }
+        return res.json({message: "Вы авторизовались."})
     }
 
     async check(req, res, next){
@@ -26,6 +33,20 @@ class UserController {
             return next(ApiError.badRequest('Не задан ID'))
         }
         res.json(query)
+    }
+
+    async getAllUsers(req, res){
+        const users = await User.findAll()
+        return res.json({message: users})
+    }
+
+    async deleteUser(req, res, next){
+        const id = req.body
+        if (!id){
+            return next(ApiError.badRequest('Пользователь для удаления не указан.'))
+        }
+        let result = await User.destroy({where: {id}})
+        return res.json({message: result})
     }
 }
 
